98f2e3bf13bb7b01aa3d1f8eeba61745d8e763a4
milenoss <milenkhanal@gmail.com>
2019-11-25 14:41:37 +0000

    Initial

--- src/App.js
+++ src/App.js
@@ -2,25 +2,25 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => { 
 
+  const APP_ID = '7351426b'
+  const APP_KEY = '6fb675de4755936ae5095c64ab2e265d	'
+  const exampleReq = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`
+
+
+
+  return (
+  <div className = 'App'>
+  
+      <form className = "search-form">
+      <input className= 'search-bar' type = 'text'></input>
+      <button className= 'search- button' type = 'submit'>
+        Search
+      </button>
+      
+    </form>
+  </div>
+  );
+  }
 export default App;
